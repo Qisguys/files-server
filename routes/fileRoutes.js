9201260ed50678a0239d65e6112c706278f50ebf
Qisguys <122888167+Qisguys@users.noreply.github.com>
2025-03-30 01:57:27 +0530

    Update fileRoutes.js

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -15,6 +15,8 @@ const upload = multer({ storage: multer.memoryStorage() });
 router.post("/upload", upload.single("file"), uploadFile);
 router.get("/", getFiles);
 router.get("/:filename", getFile);
+
+
 router.delete("/:filename", deleteFile);
 
 router.post("/upload/:userId?", upload.single("file"), uploadFiles);
