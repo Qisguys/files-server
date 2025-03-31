f25bd7e5ec87acdb355745e114667e0a645bf00b
Qisguys <122888167+Qisguys@users.noreply.github.com>
2025-03-31 16:38:42 +0530

    Update File.js

--- models/File.js
+++ models/File.js
@@ -6,7 +6,7 @@ const FileSchema = new mongoose.Schema(
     contentType: { type: String, required: true },
     size: { type: Number, required: true },
     data: { type: Buffer, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, 
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }, 
   },
   { timestamps: true }
 );
