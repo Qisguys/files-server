def24a0c7c9e4d3da8e206a0ec31f016c16754ed
Qisguys <122888167+Qisguys@users.noreply.github.com>
2025-03-31 14:23:54 +0530

    Update File.js

--- models/File.js
+++ models/File.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-const FileSchema = new mongoose.Schema({
-  filename: { type: String, required: true },
-  contentType: { type: String, required: true },
-  size: { type: Number, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true } // Reference to User model
-}, { timestamps: true });
+const FileSchema = new mongoose.Schema(
+  {
+    filename: { type: String, required: true },
+    contentType: { type: String, required: true },
+    size: { type: Number, required: true },
+    data: { type: Buffer, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, 
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("File", FileSchema);
